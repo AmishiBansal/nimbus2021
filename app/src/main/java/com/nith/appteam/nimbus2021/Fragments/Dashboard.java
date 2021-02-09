902bf63e299176814960723dfe2967a440711a89
khyatisaini <sainikhyati2001@gmail.com>
2021-02-09 23:23:37 +0530

    added progress bar abstract

--- app/src/main/java/com/nith/appteam/nimbus2021/Fragments/Dashboard.java
+++ app/src/main/java/com/nith/appteam/nimbus2021/Fragments/Dashboard.java
@@ -18,7 +18,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-import com.nith.appteam.nimbus2021.Activities.CampusAmbassador;
 import com.nith.appteam.nimbus2021.Activities.Event_Choose;
 import com.nith.appteam.nimbus2021.Activities.Exhhibition;
 import com.nith.appteam.nimbus2021.Activities.QuizMainActivity;
@@ -52,10 +51,10 @@ public class Dashboard extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        quote1 = rootView.findViewById(R.id.quote1);
-        quote2 = rootView.findViewById(R.id.quote2);
+//        quote1 = rootView.findViewById(R.id.quote1);
+//        quote2 = rootView.findViewById(R.id.quote2);
         event_text = rootView.findViewById(R.id.event_text);
-        campus_text = rootView.findViewById(R.id.campus_text);
+//        campus_text = rootView.findViewById(R.id.campus_text);
         quiz_card = rootView.findViewById(R.id.quiz_card);
         workshop_card = rootView.findViewById(R.id.workshop_card);
         exhibition_card = rootView.findViewById(R.id.exhibition_card);
@@ -89,15 +88,15 @@ public class Dashboard extends Fragment {
         omg_n.startAnimation(animation4);
         omg_k.startAnimation(animation5);
 
-        quote2.setHtml(
-                "<p>\"HOW YOU <font color=\"#2fc0d1\">CODIN'</font> \uD83D\uDCBB ?!\" "
-                        + "<small><i><font color=\"#888888\"> ~ <strike>JOEY</strike> "
-                        + "NIMBUS</font></i></small></p>");
+//        quote2.setHtml(
+//                "<p>\"HOW YOU <font color=\"#2fc0d1\">CODIN'</font> \uD83D\uDCBB ?!\" "
+//                        + "<small><i><font color=\"#888888\"> ~ <strike>JOEY</strike> "
+//                        + "NIMBUS</font></i></small></p>");
         developers_text.setHtml("<p>\"MEET THE<font color=\"#2fc0d1\"> DEVELOPERS</font> \uD83D\uDCBB\"</p>");
 
-        campus_text.setHtml(
-                "<p>\"ARE YOU A <font color=\"#2fc0d1\">CAMPUS AMBASSADOR</font> \uD83D\uDEA9"
-                        + " ?\"</p>");
+//        campus_text.setHtml(
+//                "<p>\"ARE YOU A <font color=\"#2fc0d1\">CAMPUS AMBASSADOR</font> \uD83D\uDEA9"
+//                        + " ?\"</p>");
 
 
         event_text.setHtml(
@@ -105,10 +104,10 @@ public class Dashboard extends Fragment {
                         + "!\"</p>");
 
 
-        quote1.setHtml(
-                "<p>\"I AM <strike>IRON MAN</strike> <font color=\"#2fc0d1\">SCOPUS</font>"
-                        + " \uD83D\uDE80 !\" <small><i><font color=\"#888888\"> ~ "
-                        + "NIMBUS</font></i></small></p>");
+//        quote1.setHtml(
+//                "<p>\"I AM <strike>IRON MAN</strike> <font color=\"#2fc0d1\">SCOPUS</font>"
+//                        + " \uD83D\uDE80 !\" <small><i><font color=\"#888888\"> ~ "
+//                        + "NIMBUS</font></i></small></p>");
 
         quiz_card.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -150,14 +149,14 @@ public class Dashboard extends Fragment {
                 context.overridePendingTransition(R.anim.ease_in, R.anim.ease_out);
             }
         });
-        campus_text.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getContext(), CampusAmbassador.class);
-                startActivity(i);
-                context.overridePendingTransition(R.anim.ease_in, R.anim.ease_out);
-            }
-        });
+//        campus_text.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(getContext(), CampusAmbassador.class);
+//                startActivity(i);
+//                context.overridePendingTransition(R.anim.ease_in, R.anim.ease_out);
+//            }
+//        });
         developers_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -173,22 +172,22 @@ public class Dashboard extends Fragment {
             }
         });
 
-        quote1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                alertDialogBuilder = new AlertDialog.Builder(getContext());
-                View view = getLayoutInflater().inflate(R.layout.dialog_scopus, null);
-                alertDialogBuilder.setView(view);
-                dialog = alertDialogBuilder.create();
-                dialog.show();
-//                view.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        dialog.dismiss();
-//                    }
-//                });
-            }
-        });
+//        quote1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                alertDialogBuilder = new AlertDialog.Builder(getContext());
+//                View view = getLayoutInflater().inflate(R.layout.dialog_scopus, null);
+//                alertDialogBuilder.setView(view);
+//                dialog = alertDialogBuilder.create();
+//                dialog.show();
+////                view.setOnClickListener(new View.OnClickListener() {
+////                    @Override
+////                    public void onClick(View view) {
+////                        dialog.dismiss();
+////                    }
+////                });
+//            }
+//        });
         return rootView;
     }
 }
