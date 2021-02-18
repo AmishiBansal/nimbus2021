a9bb2fb0a99df1d534e3eb0626cf82882cfc71ed
Nishant <nishantchandla6@gmail.com>
2021-02-18 14:27:06 +0530

    Register webview fixed, Images added in Event details

--- app/src/main/java/com/nith/appteam/nimbus2021/Activities/Web.java
+++ app/src/main/java/com/nith/appteam/nimbus2021/Activities/Web.java
@@ -1,5 +1,10 @@
 package com.nith.appteam.nimbus2021.Activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -22,7 +27,53 @@ public class Web extends AppCompatActivity {
         setContentView(R.layout.activity_web);
         WebView webView = findViewById(R.id.nyc_poi_webview);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                if (url.startsWith("http:") || url.startsWith("https:")) {
+                    return false;
+                } else {
+                    if (url.startsWith("intent://")) {
+                        try {
+                            Context context = webView.getContext();
+                            Intent intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
+                            if (intent != null) {
+                                PackageManager packageManager = context.getPackageManager();
+                                ResolveInfo info = packageManager.resolveActivity(intent,
+                                        PackageManager.MATCH_DEFAULT_ONLY);
+                                if ((intent != null) && ((intent.getScheme().equals("https"))
+                                        || (intent.getScheme().equals("http")))) {
+                                    String fallbackUrl = intent.getStringExtra(
+                                            "browser_fallback_url");
+                                    webView.loadUrl(fallbackUrl);
+                                    return true;
+                                }
+                                if (info != null) {
+                                    context.startActivity(intent);
+                                } else {
+                                    // Call external broswer
+                                    String fallbackUrl = intent.getStringExtra(
+                                            "browser_fallback_url");
+                                    Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                                            Uri.parse(fallbackUrl));
+                                    context.startActivity(browserIntent);
+                                }
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        } catch (Exception e) {
+                            return false;
+                        }
+                    } else {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        webView.getContext().startActivity(intent);
+                        return true;
+                    }
+                }
+            }
+        });
         webView.loadUrl(url);
 
     }
